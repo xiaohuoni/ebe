832d04a04a806d8e7a1c7210562a9e5fe29ccf65
郑敬勤 <zheng.jingqin@iwhalecloud.com>
2024-05-13 20:07:37 +0800

    feat: 提交修改

--- src/cmd/sysSetValue.ts
+++ src/cmd/sysSetValue.ts
@@ -2,22 +2,29 @@ import { parse2Var } from '../core/utils/compositeType';
 import {
   CMDGeneratorPrames,
 } from '../core/types';
+import { CMDGeneratorFunction } from '../core/utils/CMDGenerator';
 
-export function sysSetValue({ value }: CMDGeneratorPrames): string {
+export function sysSetValue({ value, platform, scope, config}: CMDGeneratorPrames): string {
   const { compId: tempCompId, valueList: temValue } = value.options;
+  const { callback1 = [] } = value; 
  let compId  = Array.isArray(tempCompId)? tempCompId : [];
+ const callback = Array.isArray(callback1) && callback1.length ?  `\n${CMDGeneratorFunction(callback1,
+  {},
+  platform,
+  scope,
+  config,
+)}` : '';
  if (typeof tempCompId === 'string' && temValue?.[tempCompId]) {
   compId = [tempCompId];
-  return `// 设置控件的值 \n setValue('${compId}', ${temValue?.[tempCompId]}) ` ;
+  return `// 设置控件的值 \n setValue('${compId}', ${parse2Var(temValue?.[tempCompId])})${callback} ` ;
  } else if (Array.isArray(tempCompId)){
   let valueList: any = {};
   compId.map((id: string ) => {
     if (temValue?.[id]) {
-      const aaa = parse2Var(temValue[id]);
-      valueList[id] = parse2Var(temValue[id]);
+      valueList[id] = temValue[id];
     }
    });
-  return `// 批量设置控件的值 \n setValue(${JSON.stringify(valueList)}) `
+  return `// 批量设置控件的值 \n setValue(${parse2Var(valueList)})${callback} `
  } else {
   return '';
  } ;
