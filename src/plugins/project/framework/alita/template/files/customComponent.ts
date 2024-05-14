3cd259eb44ba9dda3e9d2dc497231a424e207b8b
xiaohuoni <xiaohuoni@dingtalk.com>
2024-05-14 17:31:53 +0800

    chore: dependencies clearn

--- src/plugins/project/framework/alita/template/files/customComponent.ts
+++ src/plugins/project/framework/alita/template/files/customComponent.ts
@@ -25,39 +25,41 @@ export default function getFile(
   react: require('react'),
   ReactDOM: require('react-dom'),
   'react-dom': require('react-dom'),
-  classnames: require('classnames'),
-  'rc-tween-one': require('rc-tween-one'),
-  'react-player': require('react-player'),
-  'react-soundplayer': require('react-soundplayer'),
-  ahooks: require('ahooks'),
-  lodash: require('lodash'),
-  '@ant-design/icons': require('@ant-design/icons'),
-  '@lingxiteam/icons': require('@lingxiteam/icons'),
-  moment: require('moment'),
-  ${
-    isMobile
-      ? `DynamicForm: require('@lingxiteam/dform'),
-  '@lingxiteam/dform': require('@lingxiteam/dform'),
-  '@lingxiteam/antd-mobile-plus': require('@lingxiteam/antd-mobile-plus'),
-  '@lingxiteam/charts': require('@lingxiteam/charts'),
-  'ag-grid-react': require('ag-grid-react'),
-  'ag-grid-community': require('ag-grid-community'),
-  antdMobile: require('antd-mobile-5'),
-  'antd-mobile-5': require('antd-mobile-5'),
-  'rc-field-form': require('rc-field-form'),
-  '@alitajs/f2': require('@alitajs/f2'),
-  'react-loadable': require('react-loadable'),`
-      : `antd: require('antd'),
-  echarts: require('echarts'),
-  'react-quill': require('react-quill'),
-  'react-resizable': require('react-resizable'),
-  'size-sensor': require('size-sensor'),
-  'qr-code-with-logo': require('qr-code-with-logo'),
-  'echarts/core': require('echarts/core'),
-  'echarts/charts': require('echarts/charts'),
-  'echarts/components': require('echarts/components'),
-  'echarts/renderers': require('echarts/renderers'),`
-  }
+  // TODO: 依赖清理
+  // classnames: require('classnames'),
+  // 'rc-tween-one': require('rc-tween-one'),
+  // 'react-player': require('react-player'),
+  // 'react-soundplayer': require('react-soundplayer'),
+  // ahooks: require('ahooks'),
+  // lodash: require('lodash'),
+  // '@ant-design/icons': require('@ant-design/icons'),
+  // '@lingxiteam/icons': require('@lingxiteam/icons'),
+  // moment: require('moment'),
+   ${
+    ''
+  //   isMobile
+  //     ? `DynamicForm: require('@lingxiteam/dform'),
+  // '@lingxiteam/dform': require('@lingxiteam/dform'),
+  // '@lingxiteam/antd-mobile-plus': require('@lingxiteam/antd-mobile-plus'),
+  // '@lingxiteam/charts': require('@lingxiteam/charts'),
+  // 'ag-grid-react': require('ag-grid-react'),
+  // 'ag-grid-community': require('ag-grid-community'),
+  // antdMobile: require('antd-mobile-5'),
+  // 'antd-mobile-5': require('antd-mobile-5'),
+  // 'rc-field-form': require('rc-field-form'),
+  // '@alitajs/f2': require('@alitajs/f2'),
+  // 'react-loadable': require('react-loadable'),`
+  //     : `antd: require('antd'),
+  // echarts: require('echarts'),
+  // 'react-quill': require('react-quill'),
+  // 'react-resizable': require('react-resizable'),
+  // 'size-sensor': require('size-sensor'),
+  // 'qr-code-with-logo': require('qr-code-with-logo'),
+  // 'echarts/core': require('echarts/core'),
+  // 'echarts/charts': require('echarts/charts'),
+  // 'echarts/components': require('echarts/components'),
+  // 'echarts/renderers': require('echarts/renderers'),`
+   }
       };
       const Error = ({ type, fileCode }: { type: string; fileCode: string }) => {
         return (
