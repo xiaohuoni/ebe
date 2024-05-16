0bad791331acc3cb84341ceeed48300d2b474ad9
xiaohuoni <xiaohuoni@dingtalk.com>
2024-05-16 15:06:05 +0800

    fix: onOk has async

--- src/cmd/showCustomModal.ts
+++ src/cmd/showCustomModal.ts
@@ -1,15 +1,38 @@
-import { parse2Var } from '../core/utils/compositeType';
-import { CMDGeneratorFunction } from '../core/utils/CMDGenerator';
 import { CMDGeneratorPrames } from '../core/types';
-export function showCustomModal({ value, platform, scope, config }: CMDGeneratorPrames): string {
+import { CMDGeneratorFunction } from '../core/utils/CMDGenerator';
+import { parse2Var } from '../core/utils/compositeType';
+export function showCustomModal({
+  value,
+  platform,
+  scope,
+  config,
+}: CMDGeneratorPrames): string {
   const { paramsObj, modalname, pageId } = value.options;
   const { callback1 } = value;
   const params = {
     pagePath: modalname || pageId,
     params: paramsObj || {},
   };
- const onOkString = Array.isArray(callback1) && callback1.length ? `onOk: (${callback1.params[0].name}) => {
-    ${CMDGeneratorFunction(callback1, {}, platform, scope, config)}
-  }` : '\n';
-  return `ModalManagerRef?.current?.openModal({pagePath: '${params.pagePath}', lcdpParentRenderId: renderId, params: ${parse2Var(params.params)}, ${onOkString}})`;
+  const cmdFunctionString = CMDGeneratorFunction(
+    callback1,
+    {},
+    platform,
+    scope,
+    config,
+  );
+
+  const onOkString =
+    Array.isArray(callback1) && callback1.length
+      ? `onOk: ${config?.parentIsAsync ? 'async ' : ''}(${
+          // @ts-ignore
+          callback1.params[0].name
+        }) => {
+    ${cmdFunctionString}
+  }`
+      : '\n';
+  return `ModalManagerRef?.current?.openModal({pagePath: '${
+    params.pagePath
+  }', lcdpParentRenderId: renderId, params: ${parse2Var(
+    params.params,
+  )}, ${onOkString}})`;
 }
