80525886f84a38015dbc61f82280da0b331b5932
xiaohuoni <xiaohuoni@dingtalk.com>
2024-05-21 15:51:56 +0800

    fix: busicompname error

--- src/utils/schema/getBusiCompName.ts
+++ src/utils/schema/getBusiCompName.ts
@@ -2,9 +2,13 @@ import changeCase from 'change-case';
 import pinyin from 'pinyin';
 
 const busiCompMap: any = {};
+const busiCompNameMap: any = {};
 export const getBusiCompName = (busiCompMapping: any, node: any) => {
   const otherType = busiCompMapping?.[node?.props?.busiCompId] ?? '';
   try {
+    if (busiCompNameMap[otherType]) {
+      return busiCompNameMap[otherType];
+    }
     const pageName = node?.pageName ?? node?.label;
     const name = pinyin(`${pageName}`, {
       style: 'normal',
@@ -15,8 +19,12 @@ export const getBusiCompName = (busiCompMapping: any, node: any) => {
       .replaceAll('(', '')
       .replaceAll(')', '');
     if (busiCompMap[name]) {
-      return busiCompMap[name] === otherType ? name : `${name}${otherType}`;
+      const _name =
+        busiCompMap[name] === otherType ? name : `${name}${otherType}`;
+      busiCompNameMap[otherType] = _name;
+      return _name;
     }
+    busiCompNameMap[otherType] = name;
     busiCompMap[name] = otherType;
     return name;
   } catch (error) {
