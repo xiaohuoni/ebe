82c9bc306b966b108a4221a6fc2c74ec68cd8da3
LimMem <qin.chuanlong@iwhalecloud.com>
2024-05-28 21:18:17 +0800

    fix: 修改测试用例名称

--- tests/core/utils/fixSyntaxError.test.ts
+++ tests/core/utils/fixSyntaxError.test.ts
@@ -1,7 +1,7 @@
 import fixSyntaxError from '../../../src/core/utils/fixSyntaxError';
 import { expectValue } from '../../utils';
 
-describe('fixSyntaxError', () => {
+describe('给代码片段增加操作链', () => {
   test.each([
     ['from "a.b" to "a?b"', { code: 'a.b', expected: 'a?.b' }],
     ['from a["b"] to a?.["b"]', { code: 'a["b"]', expected: 'a?.["b"]' }],
