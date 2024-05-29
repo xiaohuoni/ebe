af4ec1375392b9838b9a54a3a62541e81455e87c
LimMem <qin.chuanlong@iwhalecloud.com>
2024-05-29 09:29:02 +0800

    feat: 测试用例增加函数调用?.

--- tests/core/utils/fixSyntaxError.test.ts
+++ tests/core/utils/fixSyntaxError.test.ts
@@ -3,10 +3,14 @@ import { expectValue } from '../../utils';
 
 describe('给代码片段增加操作链', () => {
   test.each([
-    ['from "a.b" to "a?b"', { code: 'a.b', expected: 'a?.b' }],
+    ['from a.b to a?b', { code: 'a.b', expected: 'a?.b' }],
     ['from a["b"] to a?.["b"]', { code: 'a["b"]', expected: 'a?.["b"]' }],
-    ['from a["b"] to a?.["b"]', { code: 'a.b()', expected: 'a?.b?.()' }],
-    ['from a["b"] to a?.["b"]', { code: 'a()', expected: 'a?.()' }],
+    ['from a.b() to a?.b?.()', { code: 'a.b()', expected: 'a?.b?.()' }],
+    ['from a() to a?.()', { code: 'a()', expected: 'a?.()' }],
+    [
+      'from a["b"]() to a?.["b"]?.()',
+      { code: 'a["b"]()', expected: 'a?.["b"]?.()' },
+    ],
   ])('%s', (name, { code, expected }) => {
     expectValue(expected, fixSyntaxError(code));
   });
