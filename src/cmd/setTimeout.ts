f9cdcc40d57f3c5234be0ca068e051ef62b5a9f4
LimMem <qin.chuanlong@iwhalecloud.com>
2024-06-03 17:21:54 +0800

    feat: 全局数据源支持设置数据源指令

--- src/cmd/setTimeout.ts
+++ src/cmd/setTimeout.ts
@@ -3,12 +3,14 @@ import { CMDGeneratorEvent } from '../core/utils/CMDGenerator';
 import { parse2Var } from '../core/utils/compositeType';
 
 export function setTimeout(generateParams: CMDGeneratorPrames): string {
-  const { value } = generateParams;
+  const { value, platform, scope, config } = generateParams;
   const { timername = 'undefined', interval, callback1 } = value.options;
   return `// 定时器
   addActionTimer('timeout', ${parse2Var(timername)}, ${CMDGeneratorEvent(
     callback1,
-    generateParams,
+    { platform },
+    scope,
+    config,
   )}, ${parse2Var(interval)});
   `;
 }
