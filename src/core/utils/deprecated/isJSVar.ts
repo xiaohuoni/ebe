52e9e090cc682acc25f97a026a0a3849d0917ddf
LimMem <qin.chuanlong@iwhalecloud.com>
2024-06-04 16:53:47 +0800

    feat: 兼容DSL旧逻辑,解析双层75121

--- src/core/utils/deprecated/isJSVar.ts
+++ src/core/utils/deprecated/isJSVar.ts
@@ -4,11 +4,22 @@ export function isJSVar(x: any) {
   return typeof x === 'string' && regex.test(x);
 }
 
+const templateRegext = /^('\$[^\x05]*\$')|("\$[^\x05]*\$")$/;
+export function isTemplateJSVar(x: any) {
+  return typeof x === 'string' && templateRegext.test(x);
+}
+
 /**
  * 解析可执行的js代码字符串
+ *
  * @param accept
  * @returns
  */
 export function parse2JSCode(accept: string) {
-  return accept.replace(/^\$|;\$|\$$/g, '');
+  let target = accept.replace(/^\$|;\$|\$$/g, '');
+  if (isTemplateJSVar(target)) {
+    // 兼容旧引擎逻辑，可能存在两层$$符号，如果只解析一层 可能代码异常
+    target = target.replace(/^["']\$|;\$|\$["']$/g, '');
+  }
+  return target;
 }
