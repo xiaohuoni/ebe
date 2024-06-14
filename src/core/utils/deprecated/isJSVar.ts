421e3fb998f5c41776c8599d9a8da46ab8af4067
LimMem <qin.chuanlong@iwhalecloud.com>
2024-06-14 18:03:28 +0800

    fix: 修复部分动态属性错误

--- src/core/utils/deprecated/isJSVar.ts
+++ src/core/utils/deprecated/isJSVar.ts
@@ -16,10 +16,18 @@ export function isTemplateJSVar(x: any) {
  * @returns
  */
 export function parse2JSCode(accept: string) {
-  let target = accept.replace(/^\$|;\$|\$$/g, '');
-  if (isTemplateJSVar(target)) {
-    // 兼容旧引擎逻辑，可能存在两层$$符号，如果只解析一层 可能代码异常
-    target = target.replace(/^["']\$|;\$|\$["']$/g, '');
+  let target = accept;
+  // 部分代码存在错误的编辑问题， 所以需要处理多层的情况
+  while (isTemplateJSVar(target) || isJSVar(target)) {
+    if (isTemplateJSVar(target)) {
+      // 兼容旧引擎逻辑，可能存在两层$$符号，如果只解析一层 可能代码异常
+      target = target.replace(/^["']\$|;\$|\$["']$/g, '');
+    }
+
+    if (isJSVar(target)) {
+      target = target.replace(/^\$|;\$|\$$/g, '');
+    }
   }
+
   return target;
 }
