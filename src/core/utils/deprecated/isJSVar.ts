27cf624d6c6e676db7f6a72dc0386eaede97d385
LimMem <qin.chuanlong@iwhalecloud.com>
2024-06-19 15:20:32 +0800

    fix: 语法解析错误

--- src/core/utils/deprecated/isJSVar.ts
+++ src/core/utils/deprecated/isJSVar.ts
@@ -4,11 +4,6 @@ export function isJSVar(x: any) {
   return typeof x === 'string' && regex.test(x);
 }
 
-const templateRegext = /^('\$[^\x05]*\$')|("\$[^\x05]*\$")$/;
-export function isTemplateJSVar(x: any) {
-  return typeof x === 'string' && templateRegext.test(x);
-}
-
 /**
  * 解析可执行的js代码字符串
  *
@@ -18,15 +13,8 @@ export function isTemplateJSVar(x: any) {
 export function parse2JSCode(accept: string) {
   let target = accept;
   // 部分代码存在错误的编辑问题， 所以需要处理多层的情况
-  while (isTemplateJSVar(target) || isJSVar(target)) {
-    if (isTemplateJSVar(target)) {
-      // 兼容旧引擎逻辑，可能存在两层$$符号，如果只解析一层 可能代码异常
-      target = target.replace(/^["']\$|;\$|\$["']$/g, '');
-    }
-
-    if (isJSVar(target)) {
-      target = target.replace(/^\$|;\$|\$$/g, '');
-    }
+  while (isJSVar(target)) {
+    target = target.replace(/^\$|;\$|\$$/g, '');
   }
 
   return target;
